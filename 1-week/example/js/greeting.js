a742631270aa9224b4e7b472914adb5b56682db4
guoyaqi <1664216915@qq.com>
2017-01-28 00:12:08 +0800

    add simple requirejs demo

---
+++ 1-week/example/js/greeting.js
@@ -0,0 +1,9 @@
+define(function(require, exports, module) {
+    module.exports = {
+        hi: hi
+    };
+
+    function hi(name) {
+        console.log('hi ' + name);
+    }
+})
---
+++ 1-week/example/js/main.js
@@ -0,0 +1,3 @@
+require(['greeting'], function(greeting) {
+	greeting.hi("qiqi");
+})
